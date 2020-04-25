c90b872551763c25e565f59178ce84a55214337f
John Chen <johnchen062094@gmail.com>
2020-04-25 18:40:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts){
   var i 
   while(i < facts.length){
-    return `${facts[i]} !!!`
+    facts[i] = facts[i] + "!!!"
     i++ 
   }
+  return facts
 }
