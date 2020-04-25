5c71991cdd5cda0d7b4376e7b1acb2797b1046a6
John Chen <johnchen062094@gmail.com>
2020-04-25 18:34:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var sentences = [] 
   var i;
   for (i = 0; i < musicians.length; i++) {
-    sentences.push(`"${musicians[i]} plays ${instruments[i]}"`) 
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`) 
   }
   return sentences
 }
